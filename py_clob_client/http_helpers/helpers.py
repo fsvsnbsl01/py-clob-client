0c210efcd1d29d8d549c9f7667a95b85322e7236
fsvsnbsl01 <129692630+fsvsnbsl01@users.noreply.github.com>
2026-03-19 10:44:40 -0300

    Update helpers.py

--- py_clob_client/http_helpers/helpers.py
+++ py_clob_client/http_helpers/helpers.py
@@ -16,7 +16,12 @@ POST = "POST"
 DELETE = "DELETE"
 PUT = "PUT"
 
-_http_client = httpx.Client(http2=True)
+import os as _os
+_proxy_url = _os.environ.get("DECODO_PROXY", "")
+_http_client = httpx.Client(
+    http2=False,
+    proxy=_proxy_url if _proxy_url else None
+)
 
 
 def overloadHeaders(method: str, headers: dict) -> dict:
